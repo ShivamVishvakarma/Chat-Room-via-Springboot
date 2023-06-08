59caeb64c4990a392706241a9e93e7be79d1e055
Shivam <vishvakarmashivam1@gmail.com>
2023-06-08 21:33:29 +0530

    init commit

---
+++ src/main/java/com/chatapp/ChatRoom/model/Message.java
@@ -0,0 +1,15 @@
+package com.chatapp.ChatRoom.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+
+    private String name;
+
+    private String Content;
+}
---
+++ src/main/resources/static/script.js
@@ -0,0 +1,80 @@
+var stompClient = null
+
+function connect(){
+
+    let socket = new SockJS("/server1")
+
+    stompClient = Stomp.over(socket)
+
+    stompClient.connect({} , function (frame){
+
+                   console.log("Connected : "+frame)
+
+                   $("name-from").addClass('d-none')
+                   $("#chat-room").removeClass('d-none')
+
+
+                     /*subscribe*/
+                   stompClient.subscribe("/topic/return-to",function(response){
+
+                      showMessage(JSON.parse(response.body))
+
+                   })
+
+
+
+    })
+
+}
+
+function sendMessgae(){
+
+let jsonOb = {
+   name: localStorage.getItem("name"),
+   content :$("#message-value").val()
+}
+
+  stompClient.send("/app/message" , {} ,  JSON.stringify(jsonOb));
+
+}
+
+
+function showMessage(message){
+
+ $("#message-container-table").prepend(`<tr><td><b>${message.name} :</b> ${message.content}</td></tr>`)
+
+}
+
+
+$(document).ready((e)=>{
+
+
+$("#login").click(()=>{
+$("#name-value").val()
+
+let name =$("#name-value").val()
+localStorage.setItem("name" , name )
+$("#name-title").html(`Welcome , <b>${name} </b>`)
+
+       connect();
+
+})
+$("#send-btn").click(()=>{
+
+   sendMessgae()
+})
+
+$("logout").click(()=>{
+
+  localStorage.removeItem("name")
+  if(stompClient!==null)
+  {
+     stompClient.disconnect()
+                        $("name-from").removeClass('d-none')
+                        $("#chat-room").addClass('d-none')
+
+  }
+})
+
+
+})
